b7cac93d58149864f4efc27709983a6a73e013ec
Yuri Sulyma <yuri.sulyma@protonmail.com>
2023-04-30 00:30:42 -0400

    fix build

--- what-study/topology-demo/vite.config.ts
+++ what-study/topology-demo/vite.config.ts
@@ -2,8 +2,15 @@ import react from "@vitejs/plugin-react-swc";
 import {defineConfig} from "vite";
 import svgr from "vite-plugin-svgr";
 
+const domain = "https://ysulyma.github.io";
+const repo = "epiplexis";
+const path = "/what-study/topology-demo";
+
+const base = `${domain}/${repo}${path}/dist`;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [
     react(),
     svgr({
