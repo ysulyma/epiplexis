084596b3356dca9148eff4a1a7398555f116088a
Yuri Sulyma <yuri.sulyma@protonmail.com>
2023-05-14 01:41:20 -0500

    gng 1.3 remove store

--- gng/1-coordinate-geometry/3-plotting/three-plotting/src/store.ts
+++
@@ -1,9 +0,0 @@
-import {create} from "zustand";
-import {subscribeWithSelector} from "zustand/middleware";
-
-export const useStore = create(
-  subscribeWithSelector(() => ({
-    line: 0,
-    circle: 0,
-  }))
-);
